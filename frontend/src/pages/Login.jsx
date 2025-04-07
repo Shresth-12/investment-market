cc161bf2a3cd05e6a06ed883676649a6ff45a246
Shresth-12 <154496143+Shresth-12@users.noreply.github.com>
2025-04-07 16:22:56 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:3000/api/v1/user/signin", {
+      const res = await axios.post("https://investment-market-backend.onrender.com/api/v1/user/signin", {
         email,
         password,
       });
